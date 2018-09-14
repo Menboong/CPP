ef637c271f324821cc326ba609f0aa784b96cc39
Menboong <ruby020415@naver.com>
2018-09-14 14:10:48 +0900

    우선순위에 의한 자료형변환

---
+++ 우선순위에 의한 자료형변환.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char word = 'a';
+	int num = 34;
+	float num2 = 212.34;
+	float sum = word+num+num2;
+	
+	cout<<"sumÀÇ °ª="<<sum<<endl;	
+} 
