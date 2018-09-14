cc64c71bf7b0baa09e48d136180ba08de906c83b
Menboong <ruby020415@naver.com>
2018-09-14 14:09:53 +0900

    cast연산자에 의한 자료형변환

---
+++ cast연산자에 의한 자료형변환.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float num1 = 2.11, num2 = 13.5;
+	cout << "num1+num2=" << num1+num2 << endl;
+	cout << "num2/num1= " << num2/num1 << endl;
+	cout << "(int)num2/(int)num1= " << (int)num2/(int)num1;
+}
