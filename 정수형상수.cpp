90ed3bded55dd3df80b062845b38df4ba3423443
Menboong <ruby020415@naver.com>
2018-09-14 14:10:55 +0900

    정수형상수

---
+++ 정수형상수.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"10진 정수형 상수="<<80<<endl;
+	cout<<"10진수로 표현한 8진 정수형 상수(0120)="<<0120<<endl;
+	cout<<"10진수로 표현한 16진수형 상수(0X50)="<<0X50<<endl;
+	return 0;
+}
