fb1d820fafef0f7ca926a1a107f6545bbe4b0d24
Menboong <ruby020415@naver.com>
2018-09-28 12:06:22 +0900

    const

---
+++ const.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define size_d 100
+const int size_c = 200;
+using namespace std;
+
+
+int main()
+{
+	int x=2;
+	int yc,yd;
+	
+	yd = x*size_d;
+	yc = x*size_c;
+	
+	cout << "yd=" << yd <<endl;
+	cout << "yc=" << yc;
+}
+
